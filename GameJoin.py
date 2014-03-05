8f1f40fed28946ac3c93d55317973d2c79fc5eee
Vadim Lobanov <valobanov@gmail.com>
2014-03-05 10:52:06 +0300

    FIXED: storing user game moved from GameJoin to GameLeave (saved games list contains just left room now)

--- GameJoin.py
+++ GameJoin.py
@@ -9,21 +9,6 @@ def GameJoin():
 	jsonRequest = request.get_json(force = True)
 	app.logger.info("hook: GameJoin: %s", jsonRequest)
 
-	if 'GameId' not in jsonRequest:
-		return json.jsonify(Message = "Missing GameId.",
-					ResultCode = 1)
-	if 'UserId' not in jsonRequest:
-		return json.jsonify(Message = "Missing UserId.",
-					ResultCode = 2)
-					
-	game_id = jsonRequest['GameId']
-	user_id = jsonRequest['UserId']
-					
-	if 'IsInactive' not in jsonRequest or not jsonRequest['IsInactive']:
-		db.delete_user_game(user_id, game_id)
-	else:
-		if 'ActorNr' in jsonRequest and jsonRequest['ActorNr'] > 0:
-			db.set_user_game(user_id, game_id, jsonRequest['ActorNr'])
-
 	return json.jsonify(Message = "",
 					ResultCode = 0)
+
--- GameLeave.py
+++ GameLeave.py
@@ -2,11 +2,29 @@ from webhooks import app
 
 from flask import request, json
 
+import db
+
 @app.route('/GameLeave', methods=['POST'])
 def GameLeave():
 	jsonRequest = request.get_json(force = True)
 	app.logger.info("hook: GameLeave: %s", jsonRequest)
-	
+
+	if 'GameId' not in jsonRequest:
+		return json.jsonify(Message = "Missing GameId.",
+					ResultCode = 1)
+	if 'UserId' not in jsonRequest:
+		return json.jsonify(Message = "Missing UserId.",
+					ResultCode = 2)
+					
+	game_id = jsonRequest['GameId']
+	user_id = jsonRequest['UserId']
+					
+	if 'IsInactive' in jsonRequest and jsonRequest['IsInactive']:
+		if 'ActorNr' in jsonRequest and jsonRequest['ActorNr'] > 0:
+			db.set_user_game(user_id, game_id, jsonRequest['ActorNr'])
+	else:
+		db.delete_user_game(user_id, game_id)
+
 	return json.jsonify(Message = "",
 					ResultCode = 0)
-
+	
