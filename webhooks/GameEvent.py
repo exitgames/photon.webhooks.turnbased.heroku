1a209ed6be682c811751fa40e15e530a42e17f30
JohnTube <brice2k5@gmail.com>
2017-09-04 15:05:26 +0100

    ADDED: saving state in GameEvent if any

--- webhooks/GameEvent.py
+++ webhooks/GameEvent.py
@@ -7,6 +7,11 @@ def GameEvent():
 	jsonRequest = request.get_json(force = True)
 	app.logger.info("hook: GameEvent: %s", jsonRequest)
 	
+    if 'State' in jsonRequest:
+		state = jsonRequest['State']
+ 		app.logger.info("hook: GameEvent: saving game: %s, state: %s", game_id, state)
+		db.set_game_state(game_id, json.dumps(state))		
+    
 	return json.jsonify(Message = "",
 					ResultCode = 0)
 
