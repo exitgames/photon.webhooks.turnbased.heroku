f801708041b5492a05a24223313740f81fd09a1c
JohnTube <brice2k5@gmail.com>
2018-07-13 16:07:06 +0100

    FIXED: GameEvent.py

--- webhooks/GameEvent.py
+++ webhooks/GameEvent.py
@@ -2,11 +2,18 @@ from app import app
 
 from flask import request, json
 
+import db
+
 @app.route('/GameEvent', methods=['POST'])
 def GameEvent():
 	jsonRequest = request.get_json(force = True)
 	app.logger.info("hook: GameEvent: %s", jsonRequest)
 	
+    if 'GameId' not in jsonRequest:
+		return json.jsonify(Message = "Missing GameId.",
+					ResultCode = 1)
+                    
+    game_id = jsonRequest['GameId']
 	if 'State' in jsonRequest:
 		state = jsonRequest['State']
 		app.logger.info("hook: GameEvent: saving game: %s, state: %s", game_id, state)
